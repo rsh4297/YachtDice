81a34b395bf24bb95d6344fbdd3d3efac41e7482
rsh4297 <74530202+rsh4297@users.noreply.github.com>
2020-12-06 23:03:10 +0900

    Update ScoreBoard.java

--- app/src/main/java/com/example/yachtdice/ScoreBoard.java
+++ app/src/main/java/com/example/yachtdice/ScoreBoard.java
@@ -1,7 +1,69 @@
 package com.example.yachtdice;
 
 public class ScoreBoard extends MainActivity {
+    private int[] countlist = new int[12];
+    private int[] samelist = new int[6];
 
+    public int[] result(int values[]) {
+
+        int small = 0;
+        int large = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i + 1 == values[j]) {
+                    samelist[i]++;
+                }
+            }
+        }
+        for (int i = 0; i < 6; i++) {              // 1~6 까지 점수
+            countlist[i] = (i + 1) * samelist[i];
+        }
+        for (int i = 0; i < 6; i++) {
+            countlist[6] += (i + 1) * samelist[i];
+        }
+        for (int i = 0; i < 6; i++) {      // 4 of a kind
+            if (samelist[i] >= 4) {
+                countlist[7] = 30;
+            }
+        }
+        for (int i = 0; i < 6; i++) {      //full house
+            if (samelist[i] == 3) {
+                for (int j = 0; j < 6; j++) {
+                    if (samelist[j] == 2) {
+                        countlist[8] = 30;
+                    }
+                }
+            }
+        }
+        for (int i=0;i<3;i++){
+            for (int j=0;j<4;j++){
+                if (samelist[i+j]==0){
+                    small += 1;
+                    break;
+                }
+            }
+        }
+        for (int i=0;i<2;i++){
+            for (int j=0;j<5;j++){
+                if(samelist[i+j]==0){
+                    large += 1;
+                    break;
+                }
+            }
+        }
+        if(small<3){                //Small Straight
+            countlist[9] = 15;
+        }
+        if(large<2){                //Large Straight
+            countlist[10] = 30;
+        }
+        for (int i=0;i<6;i++){      //Yacht
+            if(samelist[i]==5){
+                countlist[11] = 50;
+            }
+        }
+        return countlist;
+    }
 
     public void calcScore(int selectedCell) {
 
